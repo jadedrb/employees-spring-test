292d7db2ef02058b6d3f517d958054aab069e079
Jade Del Rosario-Bell <JadeDRB@Jades-MBP.lan1>
2020-09-13 17:50:41 -0400

    Updates

--- precache-manifest.4922d799570a915e62d6aa0d9aa6d2c3.js
+++ precache-manifest.4922d799570a915e62d6aa0d9aa6d2c3.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a768f6bb85bb595d7c406512bc680e43",
+    "revision": "3d19d46050a165588486a641471e527d",
     "url": "/employees-spring-test/index.html"
   },
   {
-    "revision": "a74eb3e205d27998af42",
+    "revision": "263ff60e6759dde110be",
     "url": "/employees-spring-test/static/css/main.8256d49a.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/employees-spring-test/static/js/2.87b7756a.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "a74eb3e205d27998af42",
-    "url": "/employees-spring-test/static/js/main.44bfaee1.chunk.js"
+    "revision": "263ff60e6759dde110be",
+    "url": "/employees-spring-test/static/js/main.c3031b43.chunk.js"
   },
   {
     "revision": "666e5349b6d35c1d54bf",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/employees-spring-test/precache-manifest.b69c7fb3d4f23910367dd5d9e26532de.js"
+  "/employees-spring-test/precache-manifest.4922d799570a915e62d6aa0d9aa6d2c3.js"
 );
 
 self.addEventListener('message', (event) => {
